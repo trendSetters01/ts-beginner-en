44e17c11ee3a1dfb2f6364db4a14a11f0e625061
trendSetters01 <142847732+trendSetters01@users.noreply.github.com>
2024-04-12 00:24:27 +0000

    feat: frontend

---
+++ Dao/projects/Dao/src/App.tsx
@@ -0,0 +1,72 @@
+import { DeflyWalletConnect } from '@blockshake/defly-connect'
+import { DaffiWalletConnect } from '@daffiwallet/connect'
+import { PeraWalletConnect } from '@perawallet/connect'
+import { PROVIDER_ID, ProvidersArray, WalletProvider, useInitializeProviders } from '@txnlab/use-wallet'
+import algosdk from 'algosdk'
+import { SnackbarProvider } from 'notistack'
+import Home from './Home'
+import { getAlgodConfigFromViteEnvironment, getKmdConfigFromViteEnvironment } from './utils/network/getAlgoClientConfigs'
+import * as algokit from '@algorandfoundation/algokit-utils'
+import { DaoClient } from './contracts/DaoClient'
+
+let providersArray: ProvidersArray
+if (import.meta.env.VITE_ALGOD_NETWORK === '') {
+  const kmdConfig = getKmdConfigFromViteEnvironment()
+  providersArray = [
+    {
+      id: PROVIDER_ID.KMD,
+      clientOptions: {
+        wallet: kmdConfig.wallet,
+        password: kmdConfig.password,
+        host: kmdConfig.server,
+        token: String(kmdConfig.token),
+        port: String(kmdConfig.port),
+      },
+    },
+  ]
+} else {
+  providersArray = [
+    { id: PROVIDER_ID.DEFLY, clientStatic: DeflyWalletConnect },
+    { id: PROVIDER_ID.PERA, clientStatic: PeraWalletConnect },
+    { id: PROVIDER_ID.DAFFI, clientStatic: DaffiWalletConnect },
+    { id: PROVIDER_ID.EXODUS },
+    // If you are interested in WalletConnect v2 provider
+    // refer to https://github.com/TxnLab/use-wallet for detailed integration instructions
+  ]
+}
+
+export default function App() {
+  const algodConfig = getAlgodConfigFromViteEnvironment()
+  const algodClient = algokit.getAlgoClient({
+    server: algodConfig.server,
+    port: algodConfig.port,
+    token: algodConfig.token,
+  })
+
+  const typedClient = new DaoClient(
+    {
+      resolveBy: 'id',
+      id: 0,
+    },
+    algodClient,
+  )
+
+  const walletProviders = useInitializeProviders({
+    providers: providersArray,
+    nodeConfig: {
+      network: algodConfig.network,
+      nodeServer: algodConfig.server,
+      nodePort: String(algodConfig.port),
+      nodeToken: String(algodConfig.token),
+    },
+    algosdkStatic: algosdk,
+  })
+
+  return (
+    <SnackbarProvider maxSnack={3}>
+      <WalletProvider value={walletProviders}>
+        <Home DAOtypedClient={typedClient} />
+      </WalletProvider>
+    </SnackbarProvider>
+  )
+}
---
+++ Dao/projects/Dao/src/Home.tsx
@@ -0,0 +1,57 @@
+// src/components/Home.tsx
+import { useWallet } from '@txnlab/use-wallet'
+import React, { useState } from 'react'
+import ConnectWallet from './components/ConnectWallet'
+import DaoCreateApplication from './components/DaoCreateApplication'
+import { DaoClient } from './contracts/DaoClient'
+import * as algokit from '@algorandfoundation/algokit-utils'
+
+interface HomeProps {
+  DAOtypedClient: DaoClient
+}
+
+const Home: React.FC<HomeProps> = ({ DAOtypedClient }) => {
+  const [openWalletModal, setOpenWalletModal] = useState<boolean>(false)
+  const [openDemoModal, setOpenDemoModal] = useState<boolean>(false)
+  const [proposal, setProposal] = useState<string>('This is a Phantom Pals Proposal')
+  const { activeAddress } = useWallet()
+
+  const toggleWalletModal = () => {
+    setOpenWalletModal(!openWalletModal)
+  }
+
+  const toggleDemoModal = () => {
+    setOpenDemoModal(!openDemoModal)
+  }
+
+  return (
+    <div className="hero min-h-screen bg-teal-400">
+      <div className="hero-content text-center rounded-lg p-6 max-w-md bg-white mx-auto">
+        <div className="max-w-md">
+          <h1 className="text-4xl">
+            Welcome to <div className="font-bold">the DAO</div>
+          </h1>
+          <p className="py-6">Phantom Pals Algorand DAO</p>
+
+          <div className="grid">
+            <button data-test-id="connect-wallet" className="btn m-2" onClick={toggleWalletModal}>
+              Wallet Connection
+            </button>
+            <div className="divider" />
+            <DaoCreateApplication
+              buttonClass="btn m-2"
+              buttonLoadingNode={<span className="loading loading-spinner" />}
+              buttonNode="Create DAO"
+              typedClient={DAOtypedClient}
+              proposal={proposal}
+            />
+          </div>
+
+          <ConnectWallet openModal={openWalletModal} closeModal={toggleWalletModal} />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Home
